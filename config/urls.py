385afd43e574ef5d657c40ba7bcf443aef038ac4
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-08 05:51:50 +0300

    Delete config directory

--- config/urls.py
+++
@@ -1,3 +0,0 @@
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView
-urlpatterns=[path('api/auth/login/',TokenObtainPairView.as_view()),path('api/posts/',include('posts.urls'))]
