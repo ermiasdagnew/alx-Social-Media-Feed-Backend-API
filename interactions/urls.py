df5d1daa93010a0c284896583f6454f9d043bc3a
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-08 05:52:34 +0300

    Delete interactions directory

--- interactions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import LikeCreateView, CommentListCreateView
-
-urlpatterns = [
-    path('like/', LikeCreateView.as_view(), name='like-create'),
-    path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
-]
