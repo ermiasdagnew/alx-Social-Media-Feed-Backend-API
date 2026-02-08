eddd64c2c7eaf7ef5340ae80383eab9eb3086666
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-08 05:53:20 +0300

    Delete posts directory

--- posts/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-from django.conf import settings
-class Post(models.Model): author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE); content=models.TextField()
--- posts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PostListCreateView
-
-urlpatterns = [
-    path('', PostListCreateView.as_view(), name='post-list-create'),
-]
